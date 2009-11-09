b5e9a75a155b1f62214d445022ec05581cd1e026
Yaroslav Shirokov <slava@hackinggibsons.com>
2009-11-09 01:52:55 -0500

    Logging on the logs page

---
+++ js/logs.js
@@ -0,0 +1,16 @@
+window.logs = new Logging(10);
+
+var ready = function() {
+  //Params: path to container,
+  //        relative path from container to what a row looks like
+  //        seconds between refresh
+  //        options object
+  logs.draw_some_latest("table#log", ".row", 50, {
+                          date: "%A%t g:i", //How to format a date, according to php.net/date
+                          action: {
+                            prefix: "* ",   //What to put in front of a nick performing a /me
+                            attr: 'action'  //Class to give the row of the /me
+                          }
+                        });
+};
+$(document).ready(ready);
