4bc366d599ac8a826d9e3f4c46376f378ad9ec0f
Yaroslav Shirokov <slava@hackinggibsons.com>
2009-11-09 01:34:26 -0500

    Make more sense

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 window.logs = new Logging(10);
 
 var ready = function() {
+  //Params: path to container,
+  //        relative path from container to what a row looks like
+  //        seconds between refresh
+  //        options object
   logs.draw_some_latest("table#log", ".row", 10, {
-                          date: "%A%t g:i",
+                          date: "%A%t g:i", //How to format a date, according to php.net/date
                           action: {
-                            prefix: "* ",
-                            attr: 'action'
+                            prefix: "* ",   //What to put in front of a nick performing a /me
+                            attr: 'action'  //Class to give the row of the /me
                           }
                         });
 };
