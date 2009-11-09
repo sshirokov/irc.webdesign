cdeedae9237787e6e9b8a5d07014821fd8890fc9
Yaroslav Shirokov <slava@hackinggibsons.com>
2009-11-09 02:07:41 -0500

    Might as well keep it simple

--- js/logs.js
+++ js/logs.js
@@ -1,16 +1,6 @@
 window.logs = new Logging(10); // Define refresh interval
 
 var ready = function() {
-  //Params: path to container,
-  //        relative path from container to what a row looks like
-  //        number of rows to draw
-  //        options object
-  logs.draw_some_latest("table#log", ".row", 30, {
-                          date: "%A%t g:i", //How to format a date, according to php.net/date
-                          action: {
-                            prefix: "* ",   //What to put in front of a nick performing a /me
-                            attr: 'action'  //Class to give the row of the /me
-                          }
-                        });
+  logs.draw_some_latest("table#log", ".row", 30, {date: "%A%t g:i"});
 };
 $(document).ready(ready);
