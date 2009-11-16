ea0c2fe7f12f37de09ed8ede98de10a2c6456585
Yaroslav Shirokov <slava@hackinggibsons.com>
2009-11-15 19:12:45 -0500

    Different timestamp

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.logs = new Logging(10);  // Define refresh interval
 
 var ready = function() {
-  logs.draw_some_latest("table#log", ".row", 5, {date: "%A%t g:i"});
+  logs.draw_some_latest("table#log", ".row", 5, {date: "g:ia"});
 };
 $(document).ready(ready);
